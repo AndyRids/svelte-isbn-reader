f9520c1de78a2020042c65b2bab9c66e2194ac4e
Andrew Ridyard <ridyard12@gmail.com>
2021-02-18 17:37:33 +0000

    initial App.svelte, App.test.js & index.js commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { onMount } from 'svelte';
+
+  // components
+  import NavBar from './components/navigation/NavBar.svelte';
+  import SearchISBN from './components/searching/SearchISBN.svelte';
+  import SearchAuthor from './components/searching/SearchAuthor.svelte';
+  import ScannerISBN from './components/scanning/ScannerISBN.svelte';
+  
+
+  // current displayed component name
+  let componentName = '';
+
+  // reference to imported SearchISBN, SearchAuthor & ScannerISBN components
+  const components = { SearchISBN, SearchAuthor, ScannerISBN };
+
+  onMount(() => { });
+</script>
+<NavBar bind:componentName />
+
+  <div class="wrapper-flex wrapper-flex--start">
+    
+
+    {#if componentName}
+
+      <svelte:component this={components[componentName]} bind:componentName />
+
+    {/if}
+    
+  </div>
+
+<style>
+
+</style>
